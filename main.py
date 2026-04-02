722acd472df8b81e717fd8ed9cef136274d522ba
Alexander Mcdaniel <alexandermcdaniel01@gmail.com>
2026-04-01 20:06:23 -0700

    adding more files like player and circleshape, making updates to others

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
-        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        if event.type == pygame.QUIT:
+            return
+            
+        
 
 if __name__ == "__main__":
     main()
