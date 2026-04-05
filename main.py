040456f3cc4d87ffc640b8fdf7c2d32cd98194cd
Alexander Mcdaniel <alexandermcdaniel01@gmail.com>
2026-04-05 08:41:15 -0700

    adding files

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
+        player.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+       
         
             
         
